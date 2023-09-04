81e40740f95233ca98197222f2c262d3a366af47
Rodrigo Macedo <rodmacdev@outlook.com>
2023-09-04 16:12:05 -0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,53 @@
+import logo from "./rm-logo.svg";
+import fronticon from "./fronticon.png";
+import reacticon from "./reacticon.png";
+import web from "./web.png";
+import github from "./github.png";
+import linkimg from "./linkimg.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+
+
+import omnifood from "./omnifoodproj.png";
+import todolist from "./todoproj.png";
+import quizproj from "./quizproj.png";
+import pigproj from "./pigproj.png";
+import guessproj from "./guessproj.png";
+import projetoproj from "./projproj.png";
+
+export {
+  logo,
+  fronticon,
+  reacticon,
+  web,
+  github,
+  linkimg,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  reactjs,
+  tailwind,
+  typescript,
+  threejs,
+  omnifood,
+  todolist,
+  quizproj,
+  pigproj,
+  guessproj,
+  projetoproj,
+};
